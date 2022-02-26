71b5665e42b5f9479a05fe790833e0da3de0fbc1
Dagmar Lewis <dagmar@dagmar.dev>
2022-02-26 18:24:35 -0500

    project upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('#check').addEventListener('click', check);
+
+function check() {
+    const snack = document.querySelector('#snack').value.toLowerCase();
+    if (snack === 'snack' || snack === 'damar') {
+        document.querySelector('#snacks').style.display = 'block';
+        
+    } else {
+        console.log('go away');
+    }
+}
